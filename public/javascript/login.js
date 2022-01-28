882492c0376f9bc32d308d3687648e5ba27d5bc8
Fabulosa <fabiscg79@gmail.com>
2022-01-28 12:35:05 -0500

    fix some bugs

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,29 @@
 
 
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 async function signupFormHandler(event) {
     event.preventDefault();
 
@@ -17,39 +41,9 @@ async function signupFormHandler(event) {
             }),
             headers: { 'Content-Type': 'application/json' }
         });
-        console.log(response);
 
-        // check the response status
         if (response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
-
-
-async function loginFormHandler(event) {
-    event.preventDefault();
-
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (email && password) {
-        const response = await fetch(('/api/users/login'), {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        console.log(response);
-
-        // check the response status
-        if (response.ok) {
-            document.location.raplace('/')
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
@@ -57,3 +51,5 @@ async function loginFormHandler(event) {
 }
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
